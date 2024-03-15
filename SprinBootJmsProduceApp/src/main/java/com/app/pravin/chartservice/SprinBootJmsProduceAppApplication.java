0fcf94aa964389091295e15b7290a50700f27508
pravinjenkins <pravinnikure300@gmail.com>
2024-03-15 14:44:47 +0530

    Initial Push

---
+++ SprinBootJmsProduceApp/src/main/java/com/app/pravin/chartservice/SprinBootJmsProduceAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.pravin.chartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootJmsProduceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBootJmsProduceAppApplication.class, args);
+	}
+
+}
